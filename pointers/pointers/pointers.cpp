f9b68fbf82729abfaa5db9279d15bd6a77c5d052
Noshi Anyoni <Grawiro@gmail.com>
2019-07-06 09:32:26 +0200

    done

---
+++ pointers/pointers/pointers.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include <iostream>
+
+int main ()
+{
+	int *pointer{ reinterpret_cast<int*>(0xa2ff4) };
+	std::cout << "pointer = " << reinterpret_cast<int>(pointer);
+
+	pointer = nullptr;
+	if (!pointer) {	//if (pointer != nullptr) //it's the same //pointer is 0
+		std::cout << "\npointer = " << reinterpret_cast<int>(pointer);
+	}
+}
